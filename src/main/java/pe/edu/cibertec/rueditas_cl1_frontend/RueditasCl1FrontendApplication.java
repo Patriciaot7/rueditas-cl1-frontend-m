d447e4b823afdfb4d1ed543ef339bb236463a870
Patricia <patriciaevelyn2005@gmail.com>
2024-09-25 16:52:22 -0500

    Primer avance cl1

---
+++ src/main/java/pe/edu/cibertec/rueditas_cl1_frontend/RueditasCl1FrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_cl1_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasCl1FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasCl1FrontendApplication.class, args);
+	}
+
+}
